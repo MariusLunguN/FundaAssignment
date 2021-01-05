23df4934eb5a227fead6a1659b66587b5f9e4f1c
Marius Lungu <mlu@nemetos.net>
2021-01-05 11:06:43 +0200

    Initial commit

---
+++ FundaClient/FundaClient/BusinessLogic/ApiCaller.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Configuration;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace FundaClient.BusinessLogic
+{
+    public class ApiCaller
+    {
+        private string FundaAPI => ConfigurationManager.AppSettings["FundaAPI"];
+        private string FundaKey => ConfigurationManager.AppSettings["FundaKey"];
+        
+        public JObject GetApiResponse(int i, bool hasTuin)
+        {
+            var _uri = hasTuin ? $"/?type=koop&zo=/amsterdam/tuin/&page={i}&pagesize=25" : $"/?type=koop&zo=/amsterdam/&page={i}&pagesize=25";
+            var fundaApi = string.Concat(FundaAPI, FundaKey, _uri);
+
+            var httpClient = new HttpClient();
+            Task<string> content = httpClient.GetStringAsync(fundaApi);
+
+            return JObject.Parse(content.Result);
+        }
+    }
+}
---
+++ FundaClient/FundaClient/Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using FundaClient.BusinessLogic;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace FundaClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult RichestMakelaar()
+        {
+            var sellersList = new List<int>();
+            var apiCaller = new ApiCaller();
+
+            for (int i = 1; i <= 26; i++)
+            {
+                var apiResponse = apiCaller.GetApiResponse(i, false);
+                var objectsArray = apiResponse["Objects"];
+                
+                foreach (var item in objectsArray.Children())
+                {
+                    sellersList.Add((int)item["MakelaarId"]);
+                }
+            }
+
+            ViewData["richestMakelaars"] = CommonOccurrence(sellersList.ToArray());
+
+            return View();
+        }
+
+        public ActionResult ObjectsWithTuin()
+        {
+            var sellersList = new List<int>();
+            var apiCaller = new ApiCaller();
+
+            for (int i = 1; i <= 26; i++)
+            {
+                var apiResponse = apiCaller.GetApiResponse(i, true);
+                var objectsArray = apiResponse["Objects"];
+
+                foreach (var item in objectsArray.Children())
+                {
+                    sellersList.Add((int)item["MakelaarId"]);
+                }
+            }
+
+            ViewData["objectsWithTuin"] = CommonOccurrence(sellersList.ToArray());
+
+            return View();
+        }
+
+        private static Dictionary<int, int> CommonOccurrence(int[] numbers)
+        {
+            var groups = numbers.GroupBy(x => x);
+            var largest = groups.OrderByDescending(x => x.Count()).Take(10);
+            var dict = new Dictionary<int,int>();
+
+            foreach (var item in largest)
+            {
+                dict.Add(item.Key, item.Count());
+            }
+
+            return dict;
+        }
+    }
+}
